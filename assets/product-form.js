73deea661742491fa6cf550ba019d5c2e30d3cce
Alexandre Quéré <alex.quere64@gmail.com>
2022-02-03 04:52:47 +0100

    remove useless content

--- assets/product-form.js
+++ assets/product-form.js
@@ -6,7 +6,6 @@ if (!customElements.get('product-form')) {
       this.form = this.querySelector('form');
       this.form.querySelector('[name=id]').disabled = false;
       this.form.addEventListener('submit', this.onSubmitHandler.bind(this));
-      this.cartNotification = document.querySelector('cart-notification');
     }
 
     onSubmitHandler(evt) {
